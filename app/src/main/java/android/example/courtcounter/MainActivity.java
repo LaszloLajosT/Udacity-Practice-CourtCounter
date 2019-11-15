4d7dc396ae8d4b00b216db77cfbf2f49bd7c4cb5
LaszloLajosT <track.runner800@outlook.com>
2019-11-15 15:50:40 +0000

    Ok,  I have Team A with 3 Buttons now (Score 3, 2 and 1 point). Each time when I press any of these buttons I will increase the Team A score points and show the score under the team name.

--- app/src/main/java/android/example/courtcounter/MainActivity.java
+++ app/src/main/java/android/example/courtcounter/MainActivity.java
@@ -1,14 +1,45 @@
 package android.example.courtcounter;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    int scoreTeamA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       // displayForTeamA(8);
     }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    // Increase Team A score by 3
+    public void setThree(View view) {
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    //Increase Team A score by 2
+    public void setTwo(View view) {
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    //Increase Team A score by 1
+    public void setOne(View view) {
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
+    }
+
 }
