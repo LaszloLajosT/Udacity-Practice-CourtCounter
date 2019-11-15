31ba9ff84a21d574df593920a095d52c7a74a2d9
LaszloLajosT <track.runner800@outlook.com>
2019-11-15 18:03:44 +0000

    Ok,  I have Team A and Team B with 3 Buttons now (Score 3, 2 and 1 point). Each time when I press any of these buttons I will increase the right Team  score points and show the score under the team name.

--- app/src/main/java/android/example/courtcounter/MainActivity.java
+++ app/src/main/java/android/example/courtcounter/MainActivity.java
@@ -7,39 +7,63 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    //Tracks the score for Team A
     int scoreTeamA = 0;
+    //Tracks the score for Team B
+    int scoreTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       // displayForTeamA(8);
     }
 
-    /**
-     * Displays the given score for Team A.
-     */
+    //Displays the given score for Team A.
     public void displayForTeamA(int score) {
         TextView scoreView = findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
+    
+    //Displays the given score for Team B.
+    public void displayForTeamB(int score) {
+        TextView scoreView = findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
 
-    // Increase Team A score by 3
-    public void setThree(View view) {
+    //Increase the score for Team A by 3
+    public void addThreeForTeamA(View view) {
         scoreTeamA = scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
     }
 
-    //Increase Team A score by 2
-    public void setTwo(View view) {
+    //Increase the score for Team A by 2
+    public void addTwoForTeamA(View view) {
         scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
     }
 
-    //Increase Team A score by 1
-    public void setOne(View view) {
+    //Increase the score for Team A by 1
+    public void addOneForTeamA(View view) {
         scoreTeamA = scoreTeamA + 1;
         displayForTeamA(scoreTeamA);
+    }
+
+    //Increase the score for Team B by 3
+    public void addThreeForTeamB(View view) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    //Increase the score for Team B by 2
+    public void addTwoForTeamB(View view) {
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    //Increase the score for Team B by 1
+    public void addOneForTeamB(View view) {
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
     }
 
 }
