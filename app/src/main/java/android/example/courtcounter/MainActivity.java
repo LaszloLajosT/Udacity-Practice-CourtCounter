4ef27a230355d5af75334e3696b50bc263531460
LaszloLajosT <track.runner800@outlook.com>
2019-11-16 01:23:38 +0000

    Ok,  I have Team A and Team B with 3 Buttons now (Score 3, 2 and 1 point). Each time when I press any of these buttons I will increase the right Team  score points and show the score under the team name. Added some kind of style to my code plus I tried to implement ViewModel to fix a rotate bug.

--- app/src/main/java/android/example/courtcounter/MainActivity.java
+++ app/src/main/java/android/example/courtcounter/MainActivity.java
@@ -5,17 +5,18 @@ import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProviders;
 
 public class MainActivity extends AppCompatActivity {
-    //Tracks the score for Team A
-    int scoreTeamA = 0;
-    //Tracks the score for Team B
-    int scoreTeamB = 0;
+    ScoreViewModel mViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         mViewModel = ViewModelProviders.of(this).get(ScoreViewModel.class);
+        displayForTeamA(mViewModel.scoreTeamA);
+        displayForTeamB(mViewModel.scoreTeamB);
     }
 
     //Displays the given score for Team A.
@@ -23,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         TextView scoreView = findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
     }
-    
+
     //Displays the given score for Team B.
     public void displayForTeamB(int score) {
         TextView scoreView = findViewById(R.id.team_b_score);
@@ -32,38 +33,46 @@ public class MainActivity extends AppCompatActivity {
 
     //Increase the score for Team A by 3
     public void addThreeForTeamA(View view) {
-        scoreTeamA = scoreTeamA + 3;
-        displayForTeamA(scoreTeamA);
+        mViewModel.scoreTeamA = mViewModel.scoreTeamA + 3;
+        displayForTeamA(mViewModel.scoreTeamA);
     }
 
     //Increase the score for Team A by 2
     public void addTwoForTeamA(View view) {
-        scoreTeamA = scoreTeamA + 2;
-        displayForTeamA(scoreTeamA);
+        mViewModel.scoreTeamA = mViewModel.scoreTeamA + 2;
+        displayForTeamA(mViewModel.scoreTeamA);
     }
 
     //Increase the score for Team A by 1
-    public void addOneForTeamA(View view) {
-        scoreTeamA = scoreTeamA + 1;
-        displayForTeamA(scoreTeamA);
+    public void addOneForTeamA(View v) {
+        mViewModel.scoreTeamA = mViewModel.scoreTeamA + 1;
+        displayForTeamA(mViewModel.scoreTeamA);
     }
 
     //Increase the score for Team B by 3
     public void addThreeForTeamB(View view) {
-        scoreTeamB = scoreTeamB + 3;
-        displayForTeamB(scoreTeamB);
+        mViewModel.scoreTeamB = mViewModel.scoreTeamB + 3;
+        displayForTeamB(mViewModel.scoreTeamB);
     }
 
     //Increase the score for Team B by 2
     public void addTwoForTeamB(View view) {
-        scoreTeamB = scoreTeamB + 2;
-        displayForTeamB(scoreTeamB);
+        mViewModel.scoreTeamB = mViewModel.scoreTeamB + 2;
+        displayForTeamB(mViewModel.scoreTeamB);
+    }
+    //Increase the score for Team B by 1
+    public void addOneForTeamB(View v) {
+        mViewModel.scoreTeamB = mViewModel.scoreTeamB + 1;
+        displayForTeamB(mViewModel.scoreTeamB);
     }
 
-    //Increase the score for Team B by 1
-    public void addOneForTeamB(View view) {
-        scoreTeamB = scoreTeamB + 1;
-        displayForTeamB(scoreTeamB);
+    //Reset both Team score points
+    public void resetScore(View v){
+        mViewModel.scoreTeamA =0;
+        mViewModel.scoreTeamB = 0;
+
+        displayForTeamA(mViewModel.scoreTeamA);
+        displayForTeamB(mViewModel.scoreTeamB);
     }
 
 }
