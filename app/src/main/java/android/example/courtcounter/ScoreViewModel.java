f7f903acb42a34c4eb928e7dc5aa389436390f4e
LaszloLajosT <track.runner800@outlook.com>
2020-04-24 12:50:02 +0100

    Feature: Add ViewModel

---
+++ app/src/main/java/android/example/courtcounter/ScoreViewModel.java
@@ -0,0 +1,14 @@
+package android.example.courtcounter;
+
+
+import androidx.lifecycle.ViewModel;
+
+public class ScoreViewModel extends ViewModel {
+
+    // Tracks the score for Team A
+    public int scoreTeamA = 0;
+
+    // Tracks the score for Team B
+    public int scoreTeamB = 0;
+
+}
